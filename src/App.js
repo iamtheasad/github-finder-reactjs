6a89ea041150e631c63c26ffcf49fdaea0870e6e
Md Asaduzzaman Rana <iamtheasad@gmail.com>
2024-12-26 01:24:40 +0600

    my first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-purple-500">
+      <h6 className="text-7xl">Hello World!!</h6>
+    </div>
+  );
+}
+
+export default App;
