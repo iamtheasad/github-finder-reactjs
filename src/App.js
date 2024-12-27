3233ad0c77c7f24939df81239130f9ed2993e735
Md Asaduzzaman Rana <iamtheasad@gmail.com>
2024-12-27 17:10:16 +0600

    :fire:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+
+// import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h6 className="text-7xl">Hello World!!</h6>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+          {/* <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/user/:login" exact component={User} /> */}
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
